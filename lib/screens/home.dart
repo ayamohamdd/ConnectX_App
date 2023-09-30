dc021f83e465ca51e0157d1192670e8551c2c70e
Yosra Nabil <yoyitanabil@gmail.com>
2023-10-01 02:06:55 +0300

    home ui & camera plugin

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,126 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    _controller = CameraController(
+      const CameraDescription(
+        sensorOrientation: 0,
+        name: '1',
+        lensDirection: CameraLensDirection.front,
+      ),
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+    _initializeControllerFuture = _controller.initialize();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: const Color(0xffDBDBDB),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0)),
+              ),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(30.0),
+                  bottomRight: Radius.circular(30.0),
+                ),
+                child: FutureBuilder<void>(
+                  builder: ((context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return CameraPreview(_controller);
+                    } else {
+                      return const Center(
+                        child: CircularProgressIndicator(
+                          color: Color(0xffDBDBDB),
+                        ),
+                      );
+                    }
+                  }),
+                  future: _initializeControllerFuture,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Smile To Take A Sefie :)',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: Color(0xff292929),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: screenHeight * 1 / 8,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final image = await _controller.takePicture();
+                  } catch (e) {}
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff292929),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(13.0),
+                  child: Text(
+                    'Say Cheese!',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
